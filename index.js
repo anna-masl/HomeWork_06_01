11084f1fee123b95f91fff332882632ee035ae39
anna-masl <anna.maslinnikova@gmail.com>
2025-06-25 14:16:30 +0300

    fix

--- index.js
+++ index.js
@@ -4,42 +4,33 @@
 
 const lineUserInput = prompt('Enter your text');
 const lettersUserInput = prompt('Enter letters for deleting');
+let massiveOfLetters;
 
-debugger
+debugger;
+console.log(lineUserInput, lettersUserInput);
 
-function checkAreTextString(lineUserInput) {
-  if (
-    lineUserInput === null ||
-    !lineUserInput?.trim() ||
-    typeof lineUserInput !== 'string'
-  ) {
-    return false;
-  }
-  return true;
+function isNotEmptyString(lineUserInput) {
+  return lineUserInput?.trim();
 }
-function checkAreOnlyLetters(lettersUserInput) {
-  if (
-    lettersUserInput === null ||
-    !lettersUserInput?.trim() ||
-    typeof lettersUserInput !== 'string'
-  ) {
-    return false;
+function makeMassiveOfLetters(lettersUserInput) {
+  if (isNotEmptyString(lettersUserInput)) {
+    massiveOfLetters = lettersUserInput.split('');
+    console.log(massiveOfLetters);
+    return massiveOfLetters;
   }
-  return true;
 }
 
-function getCleanRow(lineUserInput, lettersUserInput) {
-  if (
-    checkAreOnlyLetters(lettersUserInput) && checkAreTextString(lineUserInput)
-  ) {
-    const lengthOfLetters = lettersUserInput.length;
+function getCleanRow(lineUserInput, massiveOfLetters) {
+  if (isNotEmptyString(lineUserInput)) {
     let outputLine = lineUserInput;
-    for (let i = 0; i < lengthOfLetters; i++) {
-      let symbolForReplace = lettersUserInput[i];
-      outputLine = outputLine.replace(symbolForReplace, '');
-      console.log(outputLine);
+    for (let i = 0; i < lettersUserInput.length; i++) {
+      let symbolForReplace = massiveOfLetters[i];
+      outputLine = outputLine.replaceAll(symbolForReplace, '');
     }
     alert(outputLine);
+    console.log(outputLine);
   }
 }
-getCleanRow(lineUserInput, lettersUserInput);
+
+getCleanRow(lineUserInput, makeMassiveOfLetters(lettersUserInput));
+console.log(lineUserInput, massiveOfLetters);
